a290cc22fd565c36c6c20bdc454e276cd0b729d6
dilanbtk <2019141047@cumhuriyet.edu.tr>
2026-01-28 10:03:19 +0300

    create weight object model

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const seesaw = document.getElementById("seesaw");
 
+
+const objects = [];
+
 seesaw.addEventListener("click", (event) => {
   const plankRect = seesaw.getBoundingClientRect();
 
-  
+
   const clickX = event.clientX - plankRect.left;
 
-  
+
   const pivotX = plankRect.width / 2;
 
   const side = clickX < pivotX ? "left" : "right";
-
- 
   const distance = Math.abs(clickX - pivotX);
 
-  console.log("Side:", side);
-  console.log("Distance from pivot:", distance.toFixed(2), "px");
+
+  const weight = Math.floor(Math.random() * 10) + 1;
+
+  const newObject = {
+    id: Date.now(),
+    weight,
+    side,
+    distance,
+    x: clickX
+  };
+
+  objects.push(newObject);
+
+  console.log("New object added:", newObject);
+  console.log("Current state:", objects);
 });
