5335aecdc2d254348161eeb472afa9551b0dfd22
dilanbtk <2019141047@cumhuriyet.edu.tr>
2026-01-28 10:00:53 +0300

    calculate distance and  pivot

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const seesaw = document.getElementById("seesaw");
 
 seesaw.addEventListener("click", (event) => {
-  console.log("Clicked on seesaw");
-  console.log("Client X:", event.clientX);
-  console.log("Client Y:", event.clientY);
+  const plankRect = seesaw.getBoundingClientRect();
+
+  
+  const clickX = event.clientX - plankRect.left;
+
+  
+  const pivotX = plankRect.width / 2;
+
+  const side = clickX < pivotX ? "left" : "right";
+
+ 
+  const distance = Math.abs(clickX - pivotX);
+
+  console.log("Side:", side);
+  console.log("Distance from pivot:", distance.toFixed(2), "px");
 });
